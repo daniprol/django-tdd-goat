eb334beafb5daa633122506bc44c073f3b674e57
Dani Prol <prolcambados@gmail.com>
2025-01-06 10:16:17 +0100

    first passing unit test

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.test import TestCase
 
+from .views import home_page
 
-class SmokeTest(TestCase):
-    def test_bad(self):
-        self.assertEqual(1 + 1, 3)
+
+class HomePageTest(TestCase):
+    def test_home_page_returns_correct_html(self):
+        # HttpRequest is the object Django creates when a user makes a request in the browser
+        request = HttpRequest()
+        response: HttpResponse = home_page(request)
+        # Response is in bytes
+        html: str = response.content.decode("utf8")
+        self.assertIn("<title>To-Do lists</title>", html)
+        self.assertTrue(html.startswith("<html>"))
+        self.assertTrue(html.endswith("</html>"))
